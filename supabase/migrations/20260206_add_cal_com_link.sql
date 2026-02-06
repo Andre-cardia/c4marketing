db5337f096ff3340159c25e3336a5f1f8eedc58f
Andre-cardia <andre@c4marketing.com.br>
2026-02-06 08:24:38 -0300

    feat: add @calcom/embed-react dependency

---
+++ supabase/migrations/20260206_add_cal_com_link.sql
@@ -0,0 +1,5 @@
+-- Migration to add cal_com_link column to app_users table
+ALTER TABLE app_users 
+ADD COLUMN IF NOT EXISTS cal_com_link text;
+
+COMMENT ON COLUMN app_users.cal_com_link IS 'Cal.com scheduling link or username for the user';
